35ae193d74b0a49bfbf6c9308f23fb83e9f3d3bb
juanfdzz <116915178+juanfdzz@users.noreply.github.com>
2022-10-28 21:19:10 +0200

    Update altura_mayor.sh

--- altura_mayor.sh
+++ altura_mayor.sh
@@ -5,24 +5,24 @@ es_numero='^[0-9]+$'
 #Pide al menos tres parámetros y si no sale del programa
 if [ $# -le 2 ]
 then
-	echo "Debes introducir al tres un parámetro"
+	echo "Debes introducir al menos tres parámetros"
 	exit 1
 fi
 #Si el parámetro introducido no es un número entero, sale del programa
 if ! [[ $1$2$3 =~ $es_numero ]] ; then
    echo "Los parámetros deben ser números enteros" 
-   exit 1
+   exit 2
 fi
 
-if [[ $1 -gt $2 ]]; then
+if [[ $1 -gt $2 ]]; then # Si el primer parámetro es mayor que el segundo,
 
-    if [[ $1 -gt $3 ]]; then
+    if [[ $1 -gt $3 ]]; then #comprueba que también lo sea del tercero
 
         let resultado=$(($1 / 100))
         division=$(echo "scale=2; $1/100" | bc)
         echo "La mayor altura es de $division metros"
     
-    else
+    else #si no lo es, significa que el tercero es mayor.
     
         let resultado=$(($3 / 100))
         division=$(echo "scale=2; $3/100" | bc)
@@ -30,13 +30,14 @@ if [[ $1 -gt $2 ]]; then
 
     fi
 
-elif [[ $2 -gt $3 ]]; then  
+elif [[ $2 -gt $3 ]]; then  #si el primero no es mayor que el tercero, no nos interesa, así que comprueba que el segundo sea mayor que el tercero
 
     let resultado=$(($2 / 100))
     division=$(echo "scale=2; $2/100" | bc)
     echo "La mayor altura es de $division metros"
 
-else 
+else #si no lo es significa que el tercero es el mayor.
+
     let resultado=$(($3 / 100))
     division=$(echo "scale=2; $3/100" | bc)
     echo "La mayor altura es de $division metros"
